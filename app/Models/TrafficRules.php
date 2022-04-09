1d440c2cd80d18e642912eb5cc9af0198d5e7f0f
2060375 <np03cs4s210135@heraldcollege.edu.np>
2022-04-09 19:10:12 +0545

    Traffic Police and Rules file added

---
+++ app/Models/TrafficRules.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TrafficRules extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+    protected $table = "trafficrules";
+}
