39b8f4bf9d46d98530bd7dbc67bcc1728c011414
2060375 <np03cs4s210135@heraldcollege.edu.np>
2022-04-09 19:11:29 +0545

    3 model files added

---
+++ app/Models/Challan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Challan extends Model
+{
+    use HasFactory;
+
+    protected $table = "challan";
+}
