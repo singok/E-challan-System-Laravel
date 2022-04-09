37ab0be253ac5337b05db4a1633b9c94c954d59f
2060375 <np03cs4s210135@heraldcollege.edu.np>
2022-04-09 19:08:15 +0545

    Mail file added

---
+++ app/Mail/ResetMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ResetMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $email;
+    public $token;
+    public function __construct($email, $token)
+    {
+        $this->email = $email;
+        $this->token = $token;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('auths.resetForm')->with(['email' => $this->email, 'token' => $this->token]);
+    }
+}
