5054122cfceec28657bc22d617de3bcf8c555e04
2060375 <np03cs4s210135@heraldcollege.edu.np>
2022-04-21 13:46:58 +0545

    challan.blade.php file updated

--- resources/views/front/challan.blade.php
+++ resources/views/front/challan.blade.php
@@ -22,37 +22,38 @@
     
               <div class="row g-3">
                 <div class="col-md-4">
-                  <label for="inputEmail4" class="form-label">First Name</label>
-                  <input type="email" class="form-control" id="inputEmail4">
+                  <label for="inputFirstName" class="form-label">First Name</label>
+                  <input type="text" class="form-control" id="inputFirstName" name="firstName">
                 </div>
                 <div class="col-md-4">
-                  <label for="inputPassword4" class="form-label">Middle Name</label>
-                  <input type="password" class="form-control" id="inputPassword4">
+                  <label for="inputMiddleName" class="form-label">Middle Name</label>
+                  <input type="text" class="form-control" id="inputMiddleName" name="middleName">
                 </div>
                 <div class="col-md-4">
-                  <label for="inputPassword4" class="form-label">Last Name</label>
-                  <input type="password" class="form-control" id="inputPassword4">
+                  <label for="inputLastName" class="form-label">Last Name</label>
+                  <input type="text" class="form-control" id="inputLastName" name="lastName">
                 </div>
               </div>
               <div class="col-12">
                 <div class="col-md-12 py-2">
-                  <label for="inputState" class="form-label">Gender</label>
-                  <select id="inputState" class="form-select">
-                    <option selected>Choose...</option>
-                    <option>...</option>
+                  <label for="inputGender" class="form-label">Gender</label>
+                  <select id="inputGender" class="form-select" name="inputGender">
+                    <option value="" selected>Choose...</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
                   </select>
                 </div>
               </div>
               <div class="row g-3 py-2">
                 <div class="col-md-4">
                   <label for="inputAddress" class="form-label">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                <input type="text" class="form-control" id="inputAddress" name="inputAddress" placeholder="1234 Main St">
                 </div>
                 <div class="col-md-4">
-                  <label for="inputState" class="form-label">Province</label>
-                  <select id="inputState" class="form-select">
-                    <option selected>Choose...</option>
-                    <option>...</option>
+                  <label for="inputProvince" class="form-label">Province</label>
+                  <select id="inputProvince" class="form-select">
+                    <option value="" selected>Choose...</option>
+                    <option value="Province No. 1">Province No. 1</option>
                   </select>
                 </div>
                 <div class="col-md-4">
@@ -65,17 +66,17 @@
               </div>
               
               <div class="col-12 py-2">
-                <label for="inputAddress" class="form-label">Email Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="abc@gmail.com">
+                <label for="inputEmail" class="form-label">Email Address</label>
+                <input type="email" class="form-control" id="inputEmail" name="inputEmail" placeholder="abc@gmail.com">
               </div>
               <div class="row g-3 py-2">
                 <div class="col-md-6">
-                  <label for="inputAddress2" class="form-label">Mobile No.</label>
-                  <input type="text" class="form-control" id="inputAddress2" placeholder="98########">
+                  <label for="inputMobile" class="form-label">Mobile No.</label>
+                  <input type="number" class="form-control" id="inputMobile" name="inputMobile" placeholder="98########">
                 </div>
                 <div class="col-md-6">
-                  <label for="inputCity" class="form-label">Occupation</label>
-                  <input type="text" class="form-control" id="inputCity">
+                  <label for="inputOccupation" class="form-label">Occupation</label>
+                  <input type="text" class="form-control" id="inputOccupation" name="inputOccupation">
                 </div>
               </div>
               <div class="row g-3 py-4">
@@ -85,39 +86,39 @@
                   </h5>
                   <hr>
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                    <label class="form-check-label" for="flexCheckDefault">
+                    <input class="form-check-input" type="checkbox" name="inpuHealthCondition" value="Normal" id="flexCheckNormal">
+                    <label class="form-check-label" for="flexCheckNormal">
                       Normal Health Condition
                     </label>
                   </div>
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                    <label class="form-check-label" for="flexCheckChecked">
+                    <input class="form-check-input" type="checkbox" name="inpuHealthCondition" value="Blood Pressure" id="flexCheckPressure">
+                    <label class="form-check-label" for="flexCheckPressure">
                       Blood Pressure (HIGH/LOW)
                     </label>
                   </div>
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                    <label class="form-check-label" for="flexCheckChecked">
+                    <input class="form-check-input" type="checkbox" name="inpuHealthCondition" value="Heart Disease" id="flexCheckHeart">
+                    <label class="form-check-label" for="flexCheckHeart">
                       Heart Disease
                     </label>
                   </div>
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                    <label class="form-check-label" for="flexCheckDefault">
+                    <input class="form-check-input" type="checkbox" name="inpuHealthCondition" value="Kidney Disease" id="flexCheckKidney">
+                    <label class="form-check-label" for="flexCheckKidney">
                       Chronic Kidney Disease
                     </label>
                   </div>
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                    <label class="form-check-label" for="flexCheckChecked">
+                    <input class="form-check-input" type="checkbox" name="inpuHealthCondition" value="Tuberculosis" id="flexCheckIB">
+                    <label class="form-check-label" for="flexCheckTB">
                       Tuberculosis (TB)
                     </label>
                   </div>
                   
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                    <label class="form-check-label" for="flexCheckChecked">
+                    <input class="form-check-input" type="checkbox" name="inpuHealthCondition" value="Other" id="flexCheckOther">
+                    <label class="form-check-label" for="flexCheckOther">
                       Other
                     </label>
                   </div>
@@ -128,13 +129,13 @@
                   </h5>
                   <hr>
                   <div class="form-check">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" >
+                    <input class="form-check-input" type="radio" name="inputDisability" value="Yes" id="flexRadioDefault1" >
                     <label class="form-check-label" for="flexRadioDefault1">
                       Yes
                     </label>
                   </div>
                   <div class="form-check">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                    <input class="form-check-input" type="radio" name="inputDisability" value="No" id="flexRadioDefault2" checked>
                     <label class="form-check-label" for="flexRadioDefault2">
                       No
                     </label>
@@ -152,30 +153,36 @@
                 <hr>
                 <div class="row g-3">
                   <div class="col-md-4">
-                    <label for="inputEmail4" class="form-label">Plate No.</label>
-                    <input type="email" class="form-control" id="inputEmail4">
+                    <label for="inputPlateNo" class="form-label">Plate No.</label>
+                    <input type="email" class="form-control" id="inputPlateNo" name="inputPlateNo">
                   </div>
                   <div class="col-md-4">
-                    <label for="inputPassword4" class="form-label">Vehicle Type</label>
-                    <input type="password" class="form-control" id="inputPassword4">
+                    <label for="inputVehicleType" class="form-label">Vehicle Type</label>
+                    <select id="inputVehicleType" class="form-select" name="inputVehicleType">
+                      <option selected>Choose...</option>
+                      <option>A</option>
+                      <option>B</option>
+                      <option>C</option>
+                      <option>D</option>
+                    </select>
                   </div>
                   <div class="col-md-4">
-                    <label for="inputPassword4" class="form-label">Model</label>
-                    <input type="password" class="form-control" id="inputPassword4">
+                    <label for="inputModel" class="form-label">Model</label>
+                    <input type="text" class="form-control" id="inputModel" name="inputModel">
                   </div>
                 </div>
                 <div class="row g-3 py-2">
                   <div class="col-md-4">
-                    <label for="inputEmail4" class="form-label">Engine No.</label>
-                    <input type="email" class="form-control" id="inputEmail4">
+                    <label for="inputEngineNo" class="form-label">Engine No.</label>
+                    <input type="text" class="form-control" id="inputEngineNo" name="inputEngineNo">
                   </div>
                   <div class="col-md-4">
-                    <label for="inputPassword4" class="form-label">Color</label>
-                    <input type="password" class="form-control" id="inputPassword4">
+                    <label for="inputColor" class="form-label">Color</label>
+                    <input type="text" class="form-control" id="inputColor" input="inputColor">
                   </div>
                   <div class="col-md-4">
-                    <label for="inputPassword4" class="form-label">Power</label>
-                    <input type="password" class="form-control" id="inputPassword4">
+                    <label for="inputPower" class="form-label">Power</label>
+                    <input type="text" class="form-control" id="inputPower" name="inputPower">
                   </div>
                 </div>
                 <hr>
@@ -183,28 +190,37 @@
                 <hr>
                   <div class="row g-3">
                     <div class="col-md-6">
-                      <label for="inputAddress2" class="form-label">Driving License No.</label>
-                      <input type="text" class="form-control" id="inputAddress2">
+                      <label for="inputLicenseNo" class="form-label">Driving License No.</label>
+                      <input type="text" class="form-control" id="inputLicenseNo" name="inputLicenseNo">
                     </div>
                     <div class="col-md-6">
-                      <label for="inputCity" class="form-label">No. of Passengers</label>
-                      <input type="text" class="form-control" id="inputCity">
+                      <label for="inputPassengerNo" class="form-label">No. of Passengers</label>
+                      <input type="number" class="form-control" id="inputPassengerNo" name="inputPassengerNo">
                     </div>
                   </div>
                   <div class="row g-3 py-2">
                     <div class="col-md-6">
-                      <label for="inputAddress2" class="form-label">Place</label>
-                      <input type="text" class="form-control" id="inputAddress2">
+                      <label for="inputPlace" class="form-label">Place</label>
+                      <input type="text" class="form-control" id="inputPlace" name="inputPlace">
                     </div>
                     <div class="col-md-6">
-                      <label for="inputCity" class="form-label">Time</label>
-                      <input type="text" class="form-control" id="inputCity">
+                      <label for="inputTime" class="form-label">Time</label>
+                      <input type="text" class="form-control" id="inputTime" name="inputTime">
                     </div>
                   </div>
                   <div class="row g-3 py-2">
                     <div class="col-md-12">
-                      <label for="inputState" class="form-label">Reason for fine</label>
-                      <select id="inputState" class="form-select">
+                      <label for="inputPoliceGate" class="form-label">Police Gate</label>
+                      <select id="inputPoliceGate" class="form-select" name="inputPoliceGate">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="row g-3 py-2">
+                    <div class="col-md-12">
+                      <label for="inputReason" class="form-label">Reason for fine</label>
+                      <select id="inputReason" class="form-select" name="inputReason">
                         <option selected>Choose...</option>
                         <option>...</option>
                       </select>
