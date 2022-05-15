ab69769b454f476abca581f2622644fd10d5e14c
YangziSherpa <sherpayangzi7@gmail.com>
2022-05-15 15:50:58 +0545

    Vehicle list file modified

--- resources/views/pages/dashboard-vehicle-list.blade.php
+++ resources/views/pages/dashboard-vehicle-list.blade.php
@@ -34,50 +34,5 @@ use Illuminate\Support\Facades\Session;
                     <p class="card-description">
                         Here, you can find the list of all available vehicle categories.
                     </p>
-                    <div class="table-responsive">
-                        <table class="display table-striped expandable-table dataTable" style="width: 100%;" role="grid">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        Category
-                                    </th>
-                                    <th>
-                                        Category Details
-                                    </th>
-                                    <th colspan="2">
-                                        Action
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <!-- list all vehicle data -->
-                                @foreach ($dataVehicle as $info)
-                                    <tr>
-                                        <td>
-                                            {{ $info->category }}
-                                        </td>
-                                        <td>
-                                            {{ $info->details }}
-                                        </td>
-                                        <td>
-                                            <div class="btn-group" role="group" aria-label="Basic example">
-                                                <a href="{{ route('admin.vehicle-detail-update', ['id' => $info->cid]) }}"><button
-                                                        type="button"
-                                                        class="btn btn-dark btn-rounded btn-fw">Edit</button></a>
-                                                <a href="{{ route('admin.vehicle-delete-permanently', ['id' => $info->cid]) }}"><button
-                                                        type="button"
-                                                        class="btn btn-danger btn-rounded btn-fw">Delete</button></a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    {{ $dataVehicle->links() }}
-                </div>
-
-            </div>
-        </div>
-    </div>
+                    
 @endsection
